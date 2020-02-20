ae9ed50f1be8aba0723d6567a5501da2f40b434b
Andrew <a.simpson.2004@hotmail.com>
2020-02-20 19:36:33 +0000

    Remove extra line from GetServiceDetailsRequestConverter tests

--- openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/request/GetServiceDetailsRequestConverterTest.java
+++ openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/request/GetServiceDetailsRequestConverterTest.java
@@ -1,6 +1,5 @@
 package com.andrewsimpson.openldbws.converters.request;
 
-
 import com.andrewsimpson.openldbws.dto.request.GetServiceDetailsRequest;
 import com.thalesgroup.rtti._2017_10_01.ldb.GetServiceDetailsRequestParams;
 import org.junit.jupiter.api.Test;
