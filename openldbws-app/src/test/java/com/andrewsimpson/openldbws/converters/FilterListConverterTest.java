9bb339eacc1a032743045e9b88a0b5d90252c9e2
Andrew <a.simpson.2004@hotmail.com>
2020-02-21 20:25:35 +0000

    Implemented FilterListConverter tests

--- openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/FilterListConverterTest.java
+++ openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/FilterListConverterTest.java
@@ -1,7 +1,37 @@
 package com.andrewsimpson.openldbws.converters;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.thalesgroup.rtti._2017_10_01.ldb.GetDeparturesRequestParams;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 class FilterListConverterTest {
 
+    @Test
+    void from_ShouldReturnNull() {
+        // Given
+        final List<String> filterList = null;
+        // When
+        GetDeparturesRequestParams.FilterList result = FilterListConverter.from(filterList);
+        // Then
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    void from_ShouldMapAllPropertiesCorrectly() {
+        // Given
+        final List<String> filterList = new ArrayList<>();
+        filterList.add("FLTR1");
+        filterList.add("FLTR2");
+        // When
+        GetDeparturesRequestParams.FilterList result = FilterListConverter.from(filterList);
+        // Then
+        assertThat(result, is(notNullValue()));
+        assertThat(result.getCrs(), is(notNullValue()));
+        assertThat(result.getCrs().containsAll(filterList), is(equalTo(true)));
+    }
 }
