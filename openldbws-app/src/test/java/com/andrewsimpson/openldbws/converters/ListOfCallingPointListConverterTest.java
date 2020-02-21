beba6ac41998d355d7712c048da84b55166936c2
Andrew <a.simpson.2004@hotmail.com>
2020-02-21 20:48:24 +0000

    Implemented ListOfCallingPointListConverter tests

--- openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/ListOfCallingPointListConverterTest.java
+++ openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/ListOfCallingPointListConverterTest.java
@@ -1,7 +1,37 @@
 package com.andrewsimpson.openldbws.converters;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.andrewsimpson.openldbws.dto.CallingPointList;
+import com.thalesgroup.rtti._2017_10_01.ldb.types.ArrayOfArrayOfCallingPoints;
+import com.thalesgroup.rtti._2017_10_01.ldb.types.ArrayOfCallingPoints;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 class ListOfCallingPointListConverterTest {
 
+    @Test
+    void from_ShouldReturnNull() {
+        // Given
+        final ArrayOfArrayOfCallingPoints arrayOfArrayOfCallingPoints = null;
+        // When
+        List<CallingPointList> result = ListOfCallingPointListConverter.from(arrayOfArrayOfCallingPoints);
+        // Then
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    void from_ShouldMapAllPropertiesCorrectly() {
+        // Given
+        final ArrayOfArrayOfCallingPoints arrayOfArrayOfCallingPoints = new ArrayOfArrayOfCallingPoints();
+        arrayOfArrayOfCallingPoints.getCallingPointList().add(new ArrayOfCallingPoints());
+        arrayOfArrayOfCallingPoints.getCallingPointList().add(new ArrayOfCallingPoints());
+        // When
+        List<CallingPointList> result = ListOfCallingPointListConverter.from(arrayOfArrayOfCallingPoints);
+        // Then
+        assertThat(result, is(notNullValue()));
+        assertThat(result.size(), is(equalTo(2)));
+    }
 }
