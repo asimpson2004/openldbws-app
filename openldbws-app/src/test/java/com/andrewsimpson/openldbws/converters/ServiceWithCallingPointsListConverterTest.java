a97f1a34dee8a21621fc3591b0e3b2c416bcffc3
Andrew <a.simpson.2004@hotmail.com>
2020-02-23 21:43:44 +0000

    Implemented ServiceWithCallingPointsConverter and ServiceWithCallingPointsListConverter tests

--- openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/ServiceWithCallingPointsListConverterTest.java
+++ openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/ServiceWithCallingPointsListConverterTest.java
@@ -1,7 +1,39 @@
 package com.andrewsimpson.openldbws.converters;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.andrewsimpson.openldbws.dto.ServiceWithCallingPoints;
+import com.thalesgroup.rtti._2017_10_01.ldb.types.ArrayOfServiceItemsWithCallingPoints;
+import com.thalesgroup.rtti._2017_10_01.ldb.types.ServiceItemWithCallingPoints;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 class ServiceWithCallingPointsListConverterTest {
+
+    @Test
+    void from_ShouldReturnNull() {
+        // Given
+        final ArrayOfServiceItemsWithCallingPoints arrayOfServiceItemsWithCallingPoints = null;
+        // When
+        List<ServiceWithCallingPoints> result = ServiceWithCallingPointsListConverter.from(arrayOfServiceItemsWithCallingPoints);
+        // Then
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    void from_ShouldMapAllPropertiesCorrectly() {
+        // Given
+        final ArrayOfServiceItemsWithCallingPoints arrayOfServiceItemsWithCallingPoints = new ArrayOfServiceItemsWithCallingPoints();
+        arrayOfServiceItemsWithCallingPoints.getService().add(new ServiceItemWithCallingPoints());
+        arrayOfServiceItemsWithCallingPoints.getService().add(new ServiceItemWithCallingPoints());
+        arrayOfServiceItemsWithCallingPoints.getService().add(new ServiceItemWithCallingPoints());
+        // When
+        List<ServiceWithCallingPoints> result = ServiceWithCallingPointsListConverter.from(arrayOfServiceItemsWithCallingPoints);
+        // Then
+        assertThat(result, is(notNullValue()));
+        assertThat(result.size(), is(equalTo(3)));
+    }
 
 }
