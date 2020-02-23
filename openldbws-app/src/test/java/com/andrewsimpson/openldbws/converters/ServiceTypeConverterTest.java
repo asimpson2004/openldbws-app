595371d2bcf0230181c7849390b3af828608558f
Andrew <a.simpson.2004@hotmail.com>
2020-02-23 21:08:30 +0000

    Implemented ServiceTypeConverter tests

--- openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/ServiceTypeConverterTest.java
+++ openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/ServiceTypeConverterTest.java
@@ -1,7 +1,54 @@
 package com.andrewsimpson.openldbws.converters;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.andrewsimpson.openldbws.dto.ServiceType;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 class ServiceTypeConverterTest {
+
+    @Test
+    void from_ShouldReturnNull() {
+        // Given
+        final com.thalesgroup.rtti._2007_10_10.ldb.commontypes.ServiceType serviceType = null;
+        // When
+        ServiceType result = ServiceTypeConverter.from(serviceType);
+        // Then
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    void from_ShouldMapTrainCorrectly() {
+        // Given
+        final com.thalesgroup.rtti._2007_10_10.ldb.commontypes.ServiceType serviceType = com.thalesgroup.rtti._2007_10_10.ldb.commontypes.ServiceType.TRAIN;
+        // When
+        ServiceType result = ServiceTypeConverter.from(serviceType);
+        // Then
+        assertThat(result, is(notNullValue()));
+        assertThat(result, is(equalTo(ServiceType.TRAIN)));
+    }
+
+    @Test
+    void from_ShouldMapBusCorrectly() {
+        // Given
+        final com.thalesgroup.rtti._2007_10_10.ldb.commontypes.ServiceType serviceType = com.thalesgroup.rtti._2007_10_10.ldb.commontypes.ServiceType.BUS;
+        // When
+        ServiceType result = ServiceTypeConverter.from(serviceType);
+        // Then
+        assertThat(result, is(notNullValue()));
+        assertThat(result, is(equalTo(ServiceType.BUS)));
+    }
+
+    @Test
+    void from_ShouldMapFerryCorrectly() {
+        // Given
+        final com.thalesgroup.rtti._2007_10_10.ldb.commontypes.ServiceType serviceType = com.thalesgroup.rtti._2007_10_10.ldb.commontypes.ServiceType.FERRY;
+        // When
+        ServiceType result = ServiceTypeConverter.from(serviceType);
+        // Then
+        assertThat(result, is(notNullValue()));
+        assertThat(result, is(equalTo(ServiceType.FERRY)));
+    }
 
 }
