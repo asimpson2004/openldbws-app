f18d6a529e3c6d2224ea88c9a814a67ababdc485
Andrew <a.simpson.2004@hotmail.com>
2020-02-23 21:02:05 +0000

    Implemented ServiceLocationConverter and ServiceLocationListConverter tests

--- openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/ServiceLocationConverterTest.java
+++ openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/ServiceLocationConverterTest.java
@@ -1,7 +1,47 @@
 package com.andrewsimpson.openldbws.converters;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.andrewsimpson.openldbws.dto.ServiceLocation;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 class ServiceLocationConverterTest {
+
+    private final String locationName = "locationName";
+    private final String crs = "crs";
+    private final String futureChangeTo = "futureChangeTo";
+    private final String via = "via";
+    private final Boolean assocIsCancelled = false;
+
+    @Test
+    void from_ShouldReturnNull() {
+        // Given
+        final com.thalesgroup.rtti._2015_11_27.ldb.types.ServiceLocation serviceLocation = null;
+        // When
+        ServiceLocation result = ServiceLocationConverter.from(serviceLocation);
+        // Then
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    void from_ShouldMapAllPropertiesCorrectly() {
+        // Given
+        final com.thalesgroup.rtti._2015_11_27.ldb.types.ServiceLocation serviceLocation = new com.thalesgroup.rtti._2015_11_27.ldb.types.ServiceLocation();
+        serviceLocation.setLocationName(locationName);
+        serviceLocation.setAssocIsCancelled(assocIsCancelled);
+        serviceLocation.setCrs(crs);
+        serviceLocation.setFutureChangeTo(futureChangeTo);
+        serviceLocation.setVia(via);
+        // When
+        ServiceLocation result = ServiceLocationConverter.from(serviceLocation);
+        // Then
+        assertThat(result, is(notNullValue()));
+        assertThat(result.getAssocIsCancelled(), is(equalTo(assocIsCancelled)));
+        assertThat(result.getCrs(), is(equalTo(crs)));
+        assertThat(result.getFutureChangeTo(), is(equalTo(futureChangeTo)));
+        assertThat(result.getLocationName(), is(equalTo(locationName)));
+        assertThat(result.getVia(), is(equalTo(via)));
+    }
 
 }
--- openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/ServiceLocationListConverterTest.java
+++ openldbws-app/src/test/java/com/andrewsimpson/openldbws/converters/ServiceLocationListConverterTest.java
@@ -1,7 +1,36 @@
 package com.andrewsimpson.openldbws.converters;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.andrewsimpson.openldbws.dto.ServiceLocation;
+import com.thalesgroup.rtti._2015_11_27.ldb.types.ArrayOfServiceLocations;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 class ServiceLocationListConverterTest {
 
+    @Test
+    void from_ShouldReturnNull() {
+        // Given
+        final ArrayOfServiceLocations arrayOfServiceLocations = null;
+        // When
+        List<ServiceLocation> result = ServiceLocationListConverter.from(arrayOfServiceLocations);
+        // Then
+        assertThat(result, is(nullValue()));
+    }
+
+    @Test
+    void from_ShouldMapAllPropertiesCorrectly() {
+        // Given
+        final ArrayOfServiceLocations arrayOfServiceLocations = new ArrayOfServiceLocations();
+        arrayOfServiceLocations.getLocation().add(new com.thalesgroup.rtti._2015_11_27.ldb.types.ServiceLocation());
+        arrayOfServiceLocations.getLocation().add(new com.thalesgroup.rtti._2015_11_27.ldb.types.ServiceLocation());
+        // When
+        List<ServiceLocation> result = ServiceLocationListConverter.from(arrayOfServiceLocations);
+        // Then
+        assertThat(result, is(notNullValue()));
+        assertThat(result.size(), is(equalTo(2)));
+    }
 }
